7eaf2c8fa70d1ef616353ca9a7748a1d775b6adf
Tanatswa-Mabhodha <220637482@mycput.ac.za>
2026-03-19 15:26:35 +0200

    please add booking pojo

---
+++ src/main/java/domain/Booking.java
@@ -0,0 +1,113 @@
+package domain;
+
+public class Booking {
+     private String bookingID;
+     private String memberID;
+     private String carID;
+     private String startDate;
+     private String endDate;
+     private double totalCost;
+
+     private Booking(){
+
+     }
+        //builder Constructor
+     private Booking(Booking.Builder builder){
+         this.bookingID = builder.bookingID;
+         this.memberID = builder.memberID;
+         this.carID = builder.carID;
+         this.startDate = builder.startDate;
+         this.endDate = builder.endDate;
+         this.totalCost = builder.totalCost;
+     }
+
+
+    public String getBookingID() {
+        return bookingID;
+    }
+
+    public String getCarID() {
+        return carID;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingID='" + bookingID + '\'' +
+                ", memberID='" + memberID + '\'' +
+                ", carID='" + carID + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", totalCost=" + totalCost +
+                '}';
+    }
+
+    public static class Builder{
+         private String bookingID;
+         private String memberID;
+         private String carID;
+         private String startDate;
+         private String endDate;
+         private double totalCost;
+
+         public Builder setBookingID(String bookingID){
+             this.bookingID = bookingID;
+             return this;
+         }
+
+         public Builder setMemberID(String memberID){
+             this.memberID = memberID;
+             return this;
+         }
+
+         public Builder setCarID(String carID){
+             this.carID = carID;
+             return this;
+         }
+
+         public Builder setStartDate(String startDate){
+             this.startDate = startDate;
+             return this;
+         }
+
+         public Builder setEndDate(String endDate){
+             this.endDate = endDate;
+             return this;
+         }
+
+         public Builder setTotalCost(double totalCost){
+             this.totalCost = totalCost;
+             return this;
+         }
+
+         public Booking build() {
+             return new Booking(this);
+         }
+
+         }
+
+    }
+
+    /*Booking Class
+      Author: Tanatswa Geraldine Mabhodha
+      Student N0: 220637482
+      Date: 18 March 2026
+    */
+
