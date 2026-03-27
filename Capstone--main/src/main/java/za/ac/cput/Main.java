ca6bbf323963444368dc2809a58e963a73ea5c63
MalBl3ssing <230962963@mycput.ac.za>
2026-03-27 23:35:32 +0200

    Update Main.java

--- Capstone--main/src/main/java/za/ac/cput/Main.java
+++ Capstone--main/src/main/java/za/ac/cput/Main.java
@@ -1,17 +1,88 @@
 package za.ac.cput;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import za.ac.cput.domain.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(" CAR RENTAL SYSTEM ");
+
+        //  1. CUSTOMER REGISTRATION
+        Member member = new Member.Builder()
+                .setMemberId("M001")
+                .setName("Mal Samuels")
+                .setLicenseNumber("LIC12345")
+                .setPhoneNumber(0712345678)
+                .setAccountStatus("Active")
+                .build();
+
+        System.out.println("Customer Registered: " + member.getName());
+
+        // 2. CAR SELECTION 
+        Car car = new Car.Builder()
+                .setCarID("C001")
+                .setMake("Toyota")
+                .setModel("Corolla")
+                .setYear(2022)
+                .setDailyRate(500.0)
+                .setStatus("Available")
+                .setMileage(10000)
+                .build();
+
+        System.out.println("Car Selected: " + car.getMake() + " " + car.getModel());
+
+        // 3. BOOKING CREATION 
+        Booking booking = new Booking.Builder()
+                .setBookingID("B001")
+                .setMemberID(member.getMemberId())
+                .setCarID(car.getCarID())
+                .setStartDate("2026-03-27")
+                .setEndDate("2026-03-30")
+                .setTotalCost(1500.0)
+                .build();
+
+        System.out.println("Booking Created: " + booking.getBookingID());
+
+        //  4. INSURANCE ADD-ON 
+        Insurance insurance = new Insurance.Builder()
+                .setInsuranceID("I001")
+                .setBookingID(booking.getBookingID())
+                .setType("Full Cover")
+                .setDailyPremium(100.0)
+                .build();
+
+        double insuranceTotal = insurance.calculatePremiumTotal(3);
+        System.out.println("Insurance Added: " + insurance.getType() + " | Total = R" + insuranceTotal);
+
+        // 5. PAYMENT
+        Payment payment = new Payment.Builder()
+                .setPaymentID("P001")
+                .setBookingID(booking.getBookingID())
+                .setAmount(booking.getTotalCost() + insuranceTotal)
+                .setPaymentDate(LocalDate.now())
+                .setMethod("Card")
+                .build();
+
+        System.out.println(payment.generateInvoice());
+
+        //  6. BRANCH INFO 
+        Branch branch = new Branch.Builder()
+                .setBranchID(1)
+                .setBranchName("Cape Town Branch")
+                .setAddress("123 Main Road")
+                .setCity("Cape Town")
+                .setManagerName("Mr Manager")
+                .build();
+
+        branch.listAvailableCarsAtBranch();
+
+        // FINAL SUMMARY 
+        System.out.println("\n RENTAL SUMMARY ");
+        System.out.println("Customer: " + member.getName());
+        System.out.println("Car: " + car.getMake() + " " + car.getModel());
+        System.out.println("Booking ID: " + booking.getBookingID());
+        System.out.println("Total Paid: R" + payment.getAmount());
     }
 }
